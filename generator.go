ac9c1a668f25f45364f9ae7cd53391703b680e1c
Thanh Thai <thanhthai@fulygon.com>
2024-04-03 15:22:58 +0700

    update generateTotp()

--- generator.go
+++ generator.go
@@ -32,26 +32,26 @@ func generateTotp(secretKey string, timestamp int64, algorithm Algorithm, digits
 		return "", fmt.Errorf("failed to decode secret: " + err.Error())
 	}
 
-	// The truncated timestamp / 30 is converted to an 8-byte big-endian
+	// The truncated timestamp / period is converted to an 8-byte big-endian
 	// unsigned integer slice
-	// convert timestamp to bytes, divide by 30
+	// convert timestamp to bytes, divide by period
 	timeBytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(timeBytes, uint64(timestamp)/period)
 
 	// timestamp bytes are concatenated with the decoded secret key bytes
-	// then a 20-byte SHA-1 hash is calculated from the byte slice
+	// then a 20-byte hash is calculated from the byte slice
 	// and the hash with 0x0F (15) to get a single-digit offset
 	h := hmac.New(getHashInterfaces(algorithm), secretBytes)
 	h.Write(timeBytes)
 	b := h.Sum(nil)
 	offset := b[len(b)-1] & 0x0F
 
-	// truncate the SHA-1 by the offset and convert it into a 32-bit
+	// truncate hash by the offset and convert it into a 32-bit
 	// unsigned int and the 32-bit int with 0x7FFFFFFF (2147483647)
 	// to get a 31-bit unsigned int.
 	truncatedHash := binary.BigEndian.Uint32(b[offset:]) & 0x7FFFFFFF
 
-	// generate TOTP code by taking the modulo of the truncated hash
+	// return generated TOTP code by taking the modulo of the truncated hash
 	switch digits {
 	case 6:
 		return fmt.Sprintf("%06d", truncatedHash%1_000_000), nil
