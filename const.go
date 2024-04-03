d51a7a1ae53bc5fafb174c05095deff220bf7351
Thanh Thai <thanhthai@fulygon.com>
2024-04-03 17:19:33 +0700

    fix compatibility with google authenticator

--- const.go
+++ const.go
@@ -8,7 +8,7 @@ const (
 type Algorithm string
 
 const (
-	AlgorithmSHA1   Algorithm = "sha1"
-	AlgorithmSHA256 Algorithm = "sha256"
-	AlgorithmSHA512 Algorithm = "sha512"
+	AlgorithmSHA1   Algorithm = "SHA1"
+	AlgorithmSHA256 Algorithm = "SHA256"
+	AlgorithmSHA512 Algorithm = "SHA512"
 )
