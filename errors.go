a402825b2f3f7640e74a5f84dcba58f681ee76d2
Thành Thái <thanhthai@fulygon.com>
2024-04-04 18:54:41 +0700

    update errors.go

--- errors.go
+++ errors.go
@@ -9,7 +9,7 @@ var (
 	ErrEmptyAccountName = errors.New("account name cannot be empty")
 	// ErrInvalidAlgorithm invalid or unsupported algorithm
 	ErrInvalidAlgorithm = errors.New("invalid or unsupported algorithm")
-	// ErrInvalidDigits invalid or unsupported digits allowed value range is between 1 and 10
+	// ErrInvalidDigits invalid or unsupported digits, allowed value range is between 1 and 10
 	ErrInvalidDigits = errors.New("invalid or unsupported digits")
 	// ErrInvalidPeriod invalid period value
 	ErrInvalidPeriod = errors.New("period cannot be empty")
