f4d8e6072437928f0161f1f22eb1094415cdc9c1
Thanh Thai <thanhthai@fulygon.com>
2024-04-11 17:18:29 +0700

    update errors

--- errors.go
+++ errors.go
@@ -3,13 +3,13 @@ package totp
 import "errors"
 
 var (
-	// ErrEmptyIssuer issue value was required
+	// ErrEmptyIssuer value for issuer is required
 	ErrEmptyIssuer = errors.New("issuer cannot be empty")
-	// ErrEmptyAccountName account name value required
+	// ErrEmptyAccountName value for account name is required
 	ErrEmptyAccountName = errors.New("account name cannot be empty")
 	// ErrInvalidAlgorithm invalid or unsupported algorithm
 	ErrInvalidAlgorithm = errors.New("invalid or unsupported algorithm")
-	// ErrInvalidDigits invalid or unsupported digits, allowed value range is between 1 and 10
+	// ErrInvalidDigits invalid or unsupported digits, supported values are from 1 to 10
 	ErrInvalidDigits = errors.New("invalid or unsupported digits")
 	// ErrInvalidPeriod invalid period value
 	ErrInvalidPeriod = errors.New("period cannot be empty")
